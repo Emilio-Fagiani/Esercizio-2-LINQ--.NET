4fc82a49cdeabffb42d9748a585247b1e9d8ae60
emifa <emifa@LAPTOP-1TM449QB>
2023-07-03 16:08:45 +0200

    Aggiungere i file di progetto.

---
+++ Esercizio-2-LINQ--.NET/OperationWithNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio_2_LINQ__.NET
+{
+    internal class OperationWithNumber
+    {
+
+        public List<int> GenerateRandomNum(int num)
+        {
+            Random random = new Random();
+            List<int> listInt = new List<int>();
+            for(int i = 0; i < num; i++)
+            {
+                int randomNum = random.Next();
+                listInt.Add(randomNum);
+                Console.WriteLine(listInt[i]);
+            }
+            return listInt; 
+        }
+
+        public void SelectUnevenNumb(List<int> list)
+        {
+            var unevenNum = list.
+                Where(i => i % 2 != 0)
+                .OrderBy(i => i);
+            foreach(int i in unevenNum)     
+                Console.WriteLine(i);
+        }
+    }
+}
---
+++ Esercizio-2-LINQ--.NET/Program.cs
@@ -0,0 +1,14 @@
+﻿using Esercizio_2_LINQ__.NET;
+
+class Program
+{
+    static void Main()
+    {
+        OperationWithNumber operationWithNumber = new OperationWithNumber();
+        List<int> randomNum = operationWithNumber.GenerateRandomNum(10);
+        Console.WriteLine();
+        Console.WriteLine("Numeri dispari: \n");
+
+        operationWithNumber.SelectUnevenNumb(randomNum);
+    }
+}
